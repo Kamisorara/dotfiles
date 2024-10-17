edcbc2de48baaade684aac2b8cb5a8f1a5cc43ed
Kamisorara <rakamiso575@gmail.com>
2024-10-17 18:51:19 +0800

    add keymap configuration in nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -26,25 +26,40 @@ return {
         -- custom mappings
         vim.keymap.set('n', '<leader>e', api.tree.toggle, opts 'Toggle')
         vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+        vim.keymap.set('n', '<CR>', api.node.open.edit)
+        vim.keymap.set('n', 'v', api.node.open.vertical)
+        vim.keymap.set('n', 'h', api.node.open.horizontal)
+        vim.keymap.set('n', '.', api.tree.toggle_hidden_filter)
+        vim.keymap.set('n', '<F5>', api.tree.reload)
+        vim.keymap.set('n', 'a', api.fs.create)
+        vim.keymap.set('n', 'd', api.fs.remove)
+        vim.keymap.set('n', 'r', api.fs.rename)
+        vim.keymap.set('n', 'x', api.fs.cut)
+        vim.keymap.set('n', 'y', api.fs.copy.node)
+        vim.keymap.set('n', 'p', api.fs.paste)
+        vim.keymap.set('n', 's', api.node.run.system)
       end
 
       require('nvim-tree').setup {
         view = {
-          width = 35,
+          width = 30,
+          side = 'left',
+          number = false,
+          relativenumber = false,
+          signcolumn = 'yes',
         },
         on_attach = my_on_attach,
-        filters = {
-          custom = { '^.git$' },
-        },
-        actions = {
-          open_file = { quit_on_open = true },
-        },
         update_focused_file = {
           enable = true,
           update_cwd = true,
         },
         git = {
           enable = false,
+        },
+        filters = {
+          dotfiles = false,
+          custom = { 'node_modules', '.git/' },
+          exclude = { '.gitignore' },
         },
         diagnostics = {
           enable = true,
@@ -54,6 +69,12 @@ return {
             info = '',
             warning = '',
             error = '',
+          },
+        },
+        actions = {
+          open_file = {
+            resize_window = true,
+            quit_on_open = true,
           },
         },
       }
