bc7027b1870515ecf34ef4c3d85f2e7715a5a774
Kamisora <rakamiso575@gmail.com>
2025-03-29 15:45:48 +0800

    fix copilot.lua bug

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,18 +1,51 @@
 return {
   {
+    'github/copilot.vim',
+  },
+  {
     'CopilotC-Nvim/CopilotChat.nvim',
     dependencies = {
       { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
       { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
     },
     build = 'make tiktoken', -- Only on MacOS or Linux
-    -- keys = {
-    --   { '<leader>ch', '<cmd>CopilotChat<cr>', desc = 'Open Copilot Chat' },
-    --   { '<leader>ce', '<cmd>CopilotChatExplain<cr>', desc = 'Explain Code' },
-    --   { '<leader>ct', '<cmd>CopilotChatTests<cr>', desc = 'Generate Tests' },
-    --   { '<leader>cf', '<cmd>CopilotChatFix<cr>', desc = 'Fix Code' },
-    --   { '<leader>cd', '<cmd>CopilotChatDocs<cr>', desc = 'Generate Docs' },
-    -- },
+    keys = {
+      {
+        '<leader>ch',
+        '<cmd>CopilotChat<cr>',
+        desc = 'Open Copilot Chat',
+        silent = true,
+        noremap = true,
+      },
+      {
+        '<leader>ce',
+        '<cmd>CopilotChatExplain<cr>',
+        desc = 'Explain Code',
+        silent = true,
+        noremap = true,
+      },
+      {
+        '<leader>ct',
+        '<cmd>CopilotChatTests<cr>',
+        desc = 'Generate Tests',
+        silent = true,
+        noremap = true,
+      },
+      {
+        '<leader>cf',
+        '<cmd>CopilotChatFix<cr>',
+        desc = 'Fix Code',
+        silent = true,
+        noremap = true,
+      },
+      {
+        '<leader>cd',
+        '<cmd>CopilotChatDocs<cr>',
+        desc = 'Generate Docs',
+        silent = true,
+        noremap = true,
+      },
+    },
     opts = {
       -- See Configuration section for options
     },
