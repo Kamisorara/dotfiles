2476a0f48ba4aacb5b05c046ae3e80473f82e5d6
Kamisora <rakamiso575@gmail.com>
2025-03-29 11:54:05 +0800

    add github copilot plugins

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      }
+    end,
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    keys = {
+      { '<leader>ch', '<cmd>CopilotChat<cr>', desc = 'Open Copilot Chat' },
+      { '<leader>ce', '<cmd>CopilotChatExplain<cr>', desc = 'Explain Code' },
+      { '<leader>ct', '<cmd>CopilotChatTests<cr>', desc = 'Generate Tests' },
+      { '<leader>cf', '<cmd>CopilotChatFix<cr>', desc = 'Fix Code' },
+      { '<leader>cd', '<cmd>CopilotChatDocs<cr>', desc = 'Generate Docs' },
+    },
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
